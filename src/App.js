ad4b1a540ec08bdab55f527d7f17593f33f62e60
Gonzalo lledo <gonzalo_lledo@outlook.com>
2020-09-06 17:03:25 +0200

    scroll down component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 
 import Mynavbar from  './components/ui/Navbar'
+import MyCarousal from "./components/pages/carousal/Carousal"
+import TitleMsg from "./components/pages/title-carousal/title"
 
 const App = () => {
   return (
     
     <div className="App">
-      <Mynavbar></Mynavbar>
+      <MyCarousal/>
+      <Mynavbar/>
+      <TitleMsg></TitleMsg>
      My Portfolio
     </div>
     
